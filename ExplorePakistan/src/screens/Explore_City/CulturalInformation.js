b4798767baebad6b1b31af5d3142e19ed8c8242a
Muhammad Usman <muk9929106@gmail.com>
2025-03-28 19:56:12 +0500

    Complete the

--- ExplorePakistan/src/screens/Explore_City/CulturalInformation.js
+++ ExplorePakistan/src/screens/Explore_City/CulturalInformation.js
@@ -3,52 +3,63 @@ import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Sc
 import styles from './Explore_City_Style';
 
 const Cultural_Information = [
-    {
-        title: 'Local Festival',
-        details: [
-            {label:"Spring Festival", value:"1850"},
-            {label: 'Founders', value: 'Local tribal leaders' },
-            {label: 'Historical Events', value: "Timergara played a key role in regional trade and political shifts."},
-            {label:"Cultural Significance", value:"A historic trade center of Dir region",},
-            {label: 'Monuments or Landmarks', value: 'Indo-Aryan burial sites, Balambat excavation site' },
-            {label: 'Ethnic Background', value: "Primarily Pashtun"},
-            {label: 'Famous Persons', value: "Local tribal leaders, notable scholars from the region"},
-            {label: 'Historic Sites Nearby', value: "Balambat ruins, Indo-Aryan excavation sites"},
-            {label: 'Development Over Time', value: "Evolved from a historical settlement to a modern urban hub"},
-        ]
-    },
+  {
+    title: 'Local Festival',
+    details: [
+      { label: "Spring Festival", value: "Jashn-e-Bahar" },
+      { label: 'Harvest Festival', value: 'Spring Harvest Festival' },
+      { label: 'Eid Celebrations"', value: "Grand communal feasts and cultural dances" },
+    ],
+  },
+  {
+    title: 'Traditions',
+    details: [
+      { label: "Traditional Dress", value: "Shalwar Kameez with embroidered waistcoats" },
+      { label: "Hospitality", value: "Guests offered green tea and dried fruits" },
+      { label: "Storytelling Evenings", value: "Folk tales and Pashto poetry sessions" },
+      { label: "Wedding Ceremonies", value: "Traditional music and Attan dance performances" },
+    ],
+  },
+  {
+    title: 'Language_Spoken',
+    details: [
+      { label: "Primary Language", value: "Pashto" },
+      { label: "Secondary Language", value: "Urdu" },
+      { label: "Additional Languages", value: "English, Punjabi" }
+    ]
+  },
 ];
 
 const CulturalInformation = () => {
-    return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <View style={styles.container}>
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
 
-                {/* Emergency Contacts Section */}
-                <View style={styles.content}>
-                    <Text style={styles.sectionTitle}>Cultural Background</Text>
-                    
-                    <View style = {{marginBottom:60}}>
-                    {Cultural_Information.map((item, index) => (
-                        <View key={index} style={styles.cardWrapper}>
-                            <View style={styles.card}>
-                                <Text style={styles.cardTitle}>{item.title}</Text>
-                                {item.details.map((detail, i) => (
-                                    <Text key={i} style={styles.cardText}>
-                                        <Text style={styles.bold}>{detail.label}:</Text> {detail.value}
-                                    </Text>
-                                ))}
-                                <TouchableOpacity style={styles.addButton}>
-                                    <Text style={styles.addButtonText}>Add</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    ))}
-                    </View>
+        {/* Emergency Contacts Section */}
+        <View style={styles.content}>
+          <Text style={styles.sectionTitle}>Cultural Background</Text>
+
+          <View style={{ marginBottom: 60 }}>
+            {Cultural_Information.map((item, index) => (
+              <View key={index} style={styles.cardWrapper}>
+                <View style={styles.card}>
+                  <Text style={styles.cardTitle}>{item.title}</Text>
+                  {item.details.map((detail, i) => (
+                    <Text key={i} style={styles.cardText}>
+                      <Text style={styles.bold}>{detail.label}:</Text> {detail.value}
+                    </Text>
+                  ))}
+                  <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.addButtonText}>Add</Text>
+                  </TouchableOpacity>
                 </View>
-            </View>
-        </ScrollView>
-    );
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 
--- ExplorePakistan/src/screens/Explore_City/Economy.js
+++ ExplorePakistan/src/screens/Explore_City/Economy.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './Explore_City_Style';
+
+const economy = [
+  {
+            title: 'Farming', 
+            details: [
+                {label:"Major Crops", value: "Wheat, Maize, Sugarcane"},
+                {label:"Fruit Cultivation", value: "Apples, Oranges, Peaches"},
+                {label:"Live Stock", value: "Cattle, Goats, Sheep"},
+                {label:"Methods Used", value: "Traditional and modern farming techniques"}
+            ]
+            },
+            {
+             title: 'Handicrafts',
+             details: [
+                {label:"Popular Crafts", value: "Wood carving, Embroidery, Pottery"},
+                {label:"Specialty Items", value: "Traditional Pashtun embroidery, Handwoven carpets"},
+                {label:"Community Involvement", value: "Women-led embroidery businesses"}
+             ]
+          },
+          {
+            title: 'Industries' ,
+            details: [
+                {label:"Local Factories", value: "Flour mills, Textile factories"},
+                {label:"Raw Material Sources", value: "Local agriculture, Imported textile materials"},
+                {label:"Employment Contribution", value: "20% of local workforce"},
+                {label:"Economic Impact", value: "Boosts local economy and trade"}
+            ]
+          }
+];
 
 const Economy = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Explore Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+
+        {/* Emergency Contacts Section */}
+        <View style={styles.content}>
+          <Text style={styles.sectionTitle}>Economy</Text>
+
+          <View style={{ marginBottom: 60 }}>
+            {economy.map((item, index) => (
+              <View key={index} style={styles.cardWrapper}>
+                <View style={styles.card}>
+                  <Text style={styles.cardTitle}>{item.title}</Text>
+                  {item.details.map((detail, i) => (
+                    <Text key={i} style={styles.cardText}>
+                      <Text style={styles.bold}>{detail.label}:</Text> {detail.value}
+                    </Text>
+                  ))}
+                  <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.addButtonText}>Add</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+
 
 export default Economy;
