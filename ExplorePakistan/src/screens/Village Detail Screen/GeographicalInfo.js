4faf2e4a21ccfba78279f8a0a0487efdf6f54eab
Muhammad Usman <muk9929106@gmail.com>
2025-03-31 22:30:40 +0500

    Design the geographical information screen and Population Detail Screen

--- ExplorePakistan/src/screens/Village Detail Screen/GeographicalInfo.js
+++ ExplorePakistan/src/screens/Village Detail Screen/GeographicalInfo.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './VillageDetailScreenStyle';
+
+const Geographical_Info = [
+  {
+    title: 'Local Information',
+    details: [
+      { label: "Coordinates", value: "34.8296° N, 71.8316° E" },
+      { label: "Nearest Landmark", value: "Timergara Bazaar" },
+      { label: "Distance From Major City", value: "3 km from Timergara city center" }
+    ]
+  },
+  {
+    title: 'Geo Area',
+    details: [
+      { label: "Total Area", value: "5.6 sq km" },
+      { label: "Topography", value: "Mostly hilly with some plain agricultural land" },
+      { label: "Land Use", value: "Agriculture, residential areas, small commercial spaces" }
+    ]
+  },
+  {
+    title: 'Climate Detail',
+    details: [
+      {label:"Average Temperature", value: "25°C annually"},
+      {label:"Rainfall", value: "900-1100 mm per year"},
+      {label:"Seasonal Variations", value: "Hot summers (up to 40°C), mild winters (5-20°C), moderate rainfall in monsoon (July-August)"}
+    ]
+  },
+];
 
 const GeographicalInfo = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Geographical Information</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={styles.content}>
+        <Text style={styles.sectionTitle}>Geographical Information</Text>
+
+        <View style={{ marginBottom: 60 }}>
+          {Geographical_Info.map((item, index) => (
+            <View key={index} style={styles.cardWrapper}>
+              <View style={styles.card}>
+                <Text style={styles.cardTitle}>{item.title}</Text>
+                {item.details.map((detail, i) => (
+                  <Text key={i} style={styles.cardText}>
+                    <Text style={styles.bold}>{detail.label}:</Text> {detail.value}
+                  </Text>
+                ))}
+                <TouchableOpacity style={styles.addButton}>
+                  <Text style={styles.addButtonText}>Add</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView >
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+
 
 export default GeographicalInfo;
--- ExplorePakistan/src/screens/Village Detail Screen/SignificantPeaple.js
+++ ExplorePakistan/src/screens/Village Detail Screen/SignificantPeaple.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './VillageDetailScreenStyle';
+
+const Significant_Peaple = [
+  {
+    title: 'Population Children',
+    details: [
+      { label: "total", value: 3200 },
+      { label: "age_range", value: "0-14 years" },
+      { label: "percentage_of_total_population", value: "30%" }
+    ]
+  },
+  {
+    title: 'Population Adult',
+    details: [
+      { label: "Total", value: 1300 },
+      { label: "Age Range", value: "60+ years" },
+      { label: "Percentage of Total Population", value: "12%" }
+    ]
+  },
+  {
+    title: 'Gender Ratio Rate',
+    details: [
+      {label:"Male", value: 5500},
+      {label:"Female", value: 5200},
+      {label:"Male To Female Ratio", value: "1.06:1"}
+    ]
+  },
+];
 
 const SignificantPeaple = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Explore Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={styles.content}>
+        <Text style={styles.sectionTitle}>Significant Peaple</Text>
+
+        <View style={{ marginBottom: 60 }}>
+          {Significant_Peaple.map((item, index) => (
+            <View key={index} style={styles.cardWrapper}>
+              <View style={styles.card}>
+                <Text style={styles.cardTitle}>{item.title}</Text>
+                {item.details.map((detail, i) => (
+                  <Text key={i} style={styles.cardText}>
+                    <Text style={styles.bold}>{detail.label}:</Text> {detail.value}
+                  </Text>
+                ))}
+                <TouchableOpacity style={styles.addButton}>
+                  <Text style={styles.addButtonText}>Add</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView >
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+
 
 export default SignificantPeaple;
